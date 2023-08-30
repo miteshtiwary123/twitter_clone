ad4ab61ba97d7bc1948d0fd6295ec85701ce79eb
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2023-08-30 16:45:16 +0530

    created profile list frontend

--- twitter/models.py
+++ twitter/models.py
@@ -6,6 +6,7 @@ from django.db.models.signals import post_save
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
+    date_modified = models.DateTimeField(User, auto_now=True)
 
     def __str__(self) -> str:
         return self.user.username
--- twitter/views.py
+++ twitter/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Profile
 
 def home(request):
     return render(request, 'home.html')
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {"profiles": profiles})
+    else:
+        messages.success(request, ("You must be loggedin to see this page.."))
+        return redirect('home')
