71ebebbc4501bdcb28476731906371503bc1196d
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2023-08-30 15:26:40 +0530

    Automatically create user profile

--- twitter/models.py
+++ twitter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -8,4 +9,17 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+    
+
+# Create profile when new user signup
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+        # Have profile follow themselve
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
 
