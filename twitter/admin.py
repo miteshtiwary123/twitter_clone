b734967b459195b4a75b0299a9fdeae879fd10ce
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2023-08-30 14:58:46 +0530

    created user profile model

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+# Unregister Group
+admin.site.unregister(Group)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    models = User
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+# Unregister User
+admin.site.unregister(User)
+
+# Reregister User and profile
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
